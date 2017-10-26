d2de1fcb6de899788487be61e6ced63e45dc4870
Shikha <shikhapanwar1315@gmail.com>
2017-10-26 14:53:56 +0530

    controller functional

---
+++ data_path.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:52:29 10/26/2017 
+// Design Name: 
+// Module Name:    data_path 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module data_path(
+    );
+
+
+endmodule
